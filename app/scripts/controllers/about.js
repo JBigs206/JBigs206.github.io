f85bb67a7433e14c328126759442ffb9eed42675
JBigs206 <JeffreyRichardBigler@gmail.com>
2016-10-15 14:45:44 -0700

    bootstrapped project.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jbigs206githubioApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the jbigs206githubioApp
+ */
+angular.module('jbigs206githubioApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jbigs206githubioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the jbigs206githubioApp
+ */
+angular.module('jbigs206githubioApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
