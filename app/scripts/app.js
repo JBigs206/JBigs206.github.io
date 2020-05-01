d86601c0f2a03930464c2d4e23ae21a773f1eda8
JBigs206 <JeffreyRichardBigler@gmail.com>
2020-04-30 19:13:33 -0700

    Update routing for views.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -29,14 +29,33 @@ angular
       })
       .when('/about', {
         templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
+        controller: 'MainCtrl',
+        controllerAs: 'main'
       })
-      
+      .when('/projects', {
+        templateUrl: 'views/projects.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
       .when('/paybud', {
-        templateUrl: 'views/paybud.html',
+        templateUrl: 'views/project_samples/paybud.html',
         controller: 'PaybudCtrl',
         controllerAs: 'paybud'
+      })
+      .when('/aroma_yogini', {
+        templateUrl: 'views/project_samples/aroma_yogini.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/jsi', {
+        templateUrl: 'views/project_samples/jsi.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/old_ekg', {
+        templateUrl: 'views/project_samples/old_ekg.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
       })
       .otherwise({
         redirectTo: '/'
